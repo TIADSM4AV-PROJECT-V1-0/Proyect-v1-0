f5875407e70e32aec7d9101c2cb1dcb1efefd39e
ADOLFO-DEV <109006059+ADOLFO-DEV@users.noreply.github.com>
2023-11-23 02:53:56 +0000

    “[fature/fix] my comment messakkge

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,10 @@
 # example/urls.py
 from django.urls import path
 
-from example.views import index
+from example.views import index, dishes
 
 
 urlpatterns = [
     path('', index),
+    path('dishes', dishes),
 ]
